7233d4ebb8a89a4d2599c25772c1c69a3e7047d3
Evgeniy <MatyushinEN@nskes.ru>
2020-06-17 23:00:18 +0700

    lesson 2

--- script.sh
+++ script.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/bash
 
-a=$1
-b=$2
-c=$3
+read a b c
 
 # проверка что все числа и больше 0
 if [ "$a" -eq "$a" -a "$a" -gt 0 -a "$b" -eq "$b" -a "$b" -gt 0 -a "$c" -eq "$c" -a "$c" -gt 0 ] 2>>error.log;
 then	#Треугольник
 	if !(( "$a" + "$b" > "$c" & "$a" + "$c" > "$b" & "$c" + "$b" > "$a" )) ;
-		 then echo "Не треугольник"; exit 1;
+		 then echo "Не треугольник" >&2 ; exit 1 ;
 	 #Равносторонний 
 	elif [ "$a" -eq "$b" -a "$b" -eq "$c" ];
-		then echo "Равностоонний"
+		then echo "Равносторонний треугольник"
 	#Равнобедренный
 	elif [ "$a" -eq "$b" -o "$a" -eq "$c" -o "$b" -eq "$c" ] ;
-		 then echo "Равнобедренный"
+		 then echo "Равнобедренный треугольник"
 	#Tреугольник
-	else echo "Tреугольник"	
+	else echo "Обычный треугольник"	
 fi
 else 
-	echo "Ошибка: Неправильный аргумент"; tail -n1 error.log; exit 1
+	echo "Ошибка: Неправильный аргумент" >&2 ; exit 2 ;
 fi
